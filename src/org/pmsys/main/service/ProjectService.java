04ba7bf288c2a8394af713f6b3b236f1496d5166
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-05-04 15:39:55 +0800

    Added javadoc comments

--- src/org/pmsys/main/service/ProjectService.java
+++ src/org/pmsys/main/service/ProjectService.java
@@ -10,8 +10,18 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+/**
+ * Manages the persistence of projects, including saving them to the user's file
+ * and loading them from it.
+ */
 public class ProjectService extends FileService {
 
+    /**
+     * Saves a project to the current user's associated file.
+     *
+     * @param project The project to be saved.
+     * @throws IOException If an error occurs while saving the project data.
+     */
     public void saveProjectToFile(Project project) {
         try ( BufferedWriter writer = createWriterFor( getFileOfUser() ) ) {
             writer.write(project.toString());
@@ -21,10 +31,15 @@ public class ProjectService extends FileService {
         }
     }
 
+    /**
+     * Retrieves a list of projects from the current user's associated file.
+     *
+     * @return A list of projects. If an error occurs, an empty list is returned.
+     */
     public List<Project> getProjectsFromFile() {
         try (BufferedReader reader = createReaderFor( getFileOfUser() )) {
             return reader.lines()
-                    .map(Project::parseToProject)
+                    .map(Project::parseLineToProject)
                     .collect(Collectors.toList());
 
         } catch (IOException e) {
