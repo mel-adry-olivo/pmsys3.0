101e8011fff3f2015b8627bfff1c4c9bb67be70d
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-05-24 00:28:41 +0800

    refactored constraints

--- src/org/pmsys/main/ui/components/base/AbstractLayoutPanel.java
+++ src/org/pmsys/main/ui/components/base/AbstractLayoutPanel.java
@@ -9,9 +9,6 @@ public abstract class AbstractLayoutPanel extends JPanel
 
     private final MigLayout layout;
 
-    private String layoutConstraints = "";
-    private String columnConstraints = "";
-    private String rowConstraints = "";
 
     public AbstractLayoutPanel(String... constraints) {
         layout = new MigLayout();
@@ -21,33 +18,27 @@ public abstract class AbstractLayoutPanel extends JPanel
 
     @Override
     public AbstractLayoutPanel setConstraints(String... constraints) {
-        layoutConstraints = constraints[0];
-        layout.setLayoutConstraints(layoutConstraints);
+        layout.setLayoutConstraints(constraints[0]);
         if (constraints.length > 1) {
-            columnConstraints = constraints[1];
-            layout.setColumnConstraints(columnConstraints);
+            layout.setColumnConstraints(constraints[1]);
         }
         if (constraints.length > 2) {
-            rowConstraints = constraints[2];
-            layout.setRowConstraints(rowConstraints);
+            layout.setRowConstraints(constraints[2]);
         }
         return this;
     }
     @Override
     public AbstractLayoutPanel setRowConstraints(String rowConstraints) {
-        this.rowConstraints = rowConstraints;
         layout.setRowConstraints(rowConstraints);
         return this;
     }
     @Override
     public AbstractLayoutPanel setColumnConstraints(String columnConstraints) {
-        this.columnConstraints = columnConstraints;
         layout.setColumnConstraints(columnConstraints);
         return this;
     }
     @Override
     public AbstractLayoutPanel setLayoutConstraints(String layoutConstraints) {
-        this.layoutConstraints = layoutConstraints;
         layout.setLayoutConstraints(layoutConstraints);
         return this;
     }
